71c13d99fef0b676151f6dad6e2dcc8fd92a9a2c
fchristian1 <fchristian1@gmx.net>
2024-11-21 10:02:25 +0100

    add logfiles an errorhandling

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,20 @@ import hasher from 'object-hash'
 import fs from 'fs'
 
 const filename = 'rss-parser.log';
+const rssFeedsFilename = 'feeds.json';
+const rssLogFilename = 'rss.log';
+const errorLogFilename = 'error.log';
+const errorLogWriter = fs.createWriteStream(errorLogFilename, { flags: 'a' });
 let hashFile = undefined;
+let rssFeedsFile = undefined;
 
 try {
     hashFile = fs.readFileSync(filename, 'utf8');
+    rssFeedsFile = fs.readFileSync(rssFeedsFilename, 'utf8');
 } catch (err) {
     fs.writeFileSync(filename, '');
 }
+
 
 const hashArray = hashFile ? hashFile.split('\n') : [];
 const hashArrayFiltered = hashArray.filter((hash) => hash !== '');
@@ -20,20 +27,20 @@ const hashArrayFiltered = hashArray.filter((hash) => hash !== '');
 let titleHashs = hashArrayFiltered ?? [];
 
 const fileWriter = fs.createWriteStream(filename, { flags: 'a' });
+const rssLogWriter = fs.createWriteStream(rssLogFilename, { flags: 'a' });
 const parser = new rssParser();
 const counter = new promClient.Counter({ name: "test_counter", help: "test counter help", labelNames: ["name", "thema"] });
 
 const app = express();
 
-let feeds = [
-    { url: 'https://www.reddit.com/.rss', name: 'reddit' },
-    { url: 'https://www.autobild.de/rss/22590721.xml', name: 'AutoBild - Formel 1' },
-];
+let feeds = rssFeedsFile ? JSON.parse(rssFeedsFile) : [];
 
 
 
 app.use(cors());
 app.use(express.json());
+
+parsingFeedsAndCheckTitleHashs();
 
 setInterval(async () => {
     parsingFeedsAndCheckTitleHashs();
@@ -49,18 +56,17 @@ app.get("/", async (req, res) => {
 
 async function parsingFeedsAndCheckTitleHashs() {
     feeds.forEach(async (feed) => {
-
-        const parsedFeed = await parser.parseURL(feed.url);
         try {
+            const parsedFeed = await parser.parseURL(feed.url);
+            parsedFeed.items.forEach(async (item) => {
+                const titleHash = hasher(item.title);
+
+                titleHashs.includes(titleHash) ? null : await pushAndCount(feed.name, item, titleHash);
+
+            });
         } catch (error) {
-            console.log("error: ", error);
+            errorLogWriter.write(JSON.stringify(error) + "\n");
         }
-        parsedFeed.items.forEach(async (item) => {
-            const titleHash = hasher(item.title);
-
-            titleHashs.includes(titleHash) ? null : await pushAndCount(feed.name, item, titleHash);
-
-        });
     });
 }
 async function pushAndCount(feedName, item, titleHash) {
@@ -68,6 +74,8 @@ async function pushAndCount(feedName, item, titleHash) {
 
     titleHashs.push(titleHash);
     fileWriter.write(titleHash + "\n");
+    item.feedName = feedName;
+    rssLogWriter.write(JSON.stringify(item) + "\n");
 
 }
 
